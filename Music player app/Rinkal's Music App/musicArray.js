3526af087b0e840f604a058b2399cfa4f55aa67d
Rinkal Panda <153069622+Rinkal0505@users.noreply.github.com>
2024-01-16 10:27:11 +0530

    Add files via upload

---
+++ Music player app/Rinkal's Music App/musicArray.js
@@ -0,0 +1,26 @@
+let allMusic = [
+    {
+        name: "Music-1",
+        artist: "Artist-1",
+        img: "music-1",
+        src: "music-1",
+    },
+    {
+        name: "Music-2",
+        artist: "Artist-2",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Music-3",
+        artist: "Artist-3",
+        img: "music-3",
+        src: "music-3",
+    },
+    {
+        name: "Music-4",
+        artist: "Artist-4",
+        img: "music-4",
+        src: "music-4",
+    },
+];
